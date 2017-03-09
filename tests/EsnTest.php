99f626ba351270d83c9e3e7861cd471f16b1ec95
Don Cranford <dcranford@katalystsolutions.com>
2017-03-09 09:19:18 -0600

    WIP - sketching out tests to write for Esn class

---
+++ tests/EsnTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author  donaldcranford
+ * @created 3/9/17 9:11 AM
+ */
+
+namespace Katalyst\MdnEsn\Test;
+
+
+class EsnTest extends \PHPUnit_Framework_TestCase
+{
+	public function submitted_esn_is_valid_format()
+	{
+
+	}
+
+	public function submitted_esn_is_not_valid_format()
+	{
+
+	}
+}
