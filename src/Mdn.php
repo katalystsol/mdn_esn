cbe0437cbf0e78c5da4e29aeaaa924d9d49c3bd1
Don Cranford <dcranford@katalystsolutions.com>
2017-03-09 08:31:42 -0600

    Initial commit of Mdn class for testing

---
+++ src/Mdn.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @author  donaldcranford
+ * @created 3/9/17 8:18 AM
+ */
+
+namespace Katalyst\MdnEsn;
+
+/**
+ * Class Mdn
+ *
+ * This class handles Mobile Device Numbers format validation
+ *
+ * @package Katalyst\MdnEsn
+ */
+class Mdn
+{
+	/** @var  string The submitted MDN */
+	protected $submitted_mdn;
+
+	/** @var int The length of the mobile device number */
+	protected $mdn_length;
+
+	/** @var int The minimum length allowed for MDN */
+	protected $minimum_mdn_length = 6;
+
+	/**
+	 * @param string $mdn
+	 * @param int $mdn_length
+	 */
+	public function __construct($mdn, $mdn_length = 10)
+	{
+		$this->submitted_mdn	= $mdn;
+		$this->formatted_mdn	= $this->formatMdn();
+
+		$this->setMdnLength($mdn_length);
+	}
+
+	public function __toString()
+	{
+		return $this->formatted_mdn;
+	}
+
+	public function getFormattedMDN()
+	{
+		return $this->formatted_mdn;
+	}
+
+	public function getLength()
+	{
+		return $this->mdn_length;
+	}
+
+	/**
+	 * Checks if the MDN is the correct length
+	 * @return bool		TRUE if correct length
+	 */
+	public function isValid()
+	{
+		if (strlen($this->formatted_mdn) == $this->mdn_length)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Sets the mdn_length property
+	 *
+	 * @param int $length
+	 */
+	protected function setMdnLength($length)
+	{
+		$length = (int) $length;
+		if ($length > $this->minimum_mdn_length) {
+			$this->mdn_length = $length;
+			return;
+		}
+
+		$this->mdn_length = $this->minimum_mdn_length;
+	}
+
+	/**
+	 * Strips all non-numeric characters from the number
+	 *
+	 * @return string
+	 */
+	protected function formatMdn()
+	{
+		return preg_replace( "/[^0-9]/", "", $this->submitted_mdn);
+	}
+
+}
