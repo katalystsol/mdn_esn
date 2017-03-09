ca4a4dfee9aaa41a062cb1064b78048d46169deb
Don Cranford <dcranford@katalystsolutions.com>
2017-03-09 09:41:59 -0600

    Initial tests for Esn

--- tests/EsnTest.php
+++ tests/EsnTest.php
@@ -6,15 +6,70 @@
 
 namespace Katalyst\MdnEsn\Test;
 
+use Katalyst\MdnEsn\Esn;
 
 class EsnTest extends \PHPUnit_Framework_TestCase
 {
-	public function submitted_esn_is_valid_format()
+	/** @test */
+	public function submitted_11_digit_esn_is_valid_format()
+	{
+		$esn_number = '23981037459';
+		$esn = new Esn($esn_number);
+
+		$this->assertTrue($esn->isValidFormat());
+	}
+
+	/** @test */
+	public function submitted_10_digit_esn_is_not_valid_format()
+	{
+		$esn_number = '2398103745';
+		$esn = new Esn($esn_number);
+
+		$this->assertFalse($esn->isValidFormat());
+	}
+
+	/** @test */
+	public function submitted_14_character_meid_is_valid_format()
+	{
+		$meid_number = 'A10000009296F2';
+		$esn = new Esn($meid_number);
+
+		$this->assertTrue($esn->isValidFormat());
+	}
+
+	/** @test */
+	public function submitted_15_character_meid_is_not_valid_format()
+	{
+		$meid_number = 'A10000009296F2X';
+		$esn = new Esn($meid_number);
+
+		$this->assertFalse($esn->isValidFormat());
+	}
+
+	/** @test */
+	public function submitted_18_character_meid_is_valid_format()
+	{
+		$meid_number = '268435456010201020';
+		$esn = new Esn($meid_number);
+
+		$this->assertTrue($esn->isValidFormat());
+	}
+
+	/** @test */
+	public function submitted_19_character_meid_is_valid_format()
+	{
+		$meid_number = '2684354560102010200';
+		$esn = new Esn($meid_number);
+
+		$this->assertFalse($esn->isValidFormat());
+	}
+
+	public function returns_proper_hex_format()
 	{
 
 	}
 
-	public function submitted_esn_is_not_valid_format()
+	public function returns_proper_dec_format()
 	{
 
 	}
