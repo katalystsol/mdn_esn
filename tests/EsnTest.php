9250f7fddb00aa3622baf466f469571dc3d66451
Don Cranford <dcranford@katalystsolutions.com>
2017-03-10 08:39:44 -0600

    Add additional tests for Esn class

--- tests/EsnTest.php
+++ tests/EsnTest.php
@@ -56,7 +56,7 @@ class EsnTest extends \PHPUnit_Framework_TestCase
 	}
 
 	/** @test */
-	public function submitted_19_character_meid_is_valid_format()
+	public function submitted_19_character_meid_is_not_valid_format()
 	{
 		$meid_number = '2684354560102010200';
 		$esn = new Esn($meid_number);
@@ -64,13 +64,41 @@ class EsnTest extends \PHPUnit_Framework_TestCase
 		$this->assertFalse($esn->isValidFormat());
 	}
 
-	public function returns_proper_hex_format()
+	/** @test */
+	public function returns_proper_hex_format_when_submitted_in_hex()
 	{
+		$meid_number = 'A10000009296F2';
+		$esn = new Esn($meid_number);
 
+		$this->assertEquals($meid_number, $esn->getHex());
 	}
 
-	public function returns_proper_dec_format()
+	/** @future */
+	public function returns_proper_hex_format_when_submitted_in_dec()
 	{
+		$meid_number = '268435456010201020';
+		$hex_format = 'A00000009BA7BC';
+		$esn = new Esn($meid_number);
 
+		$this->assertEquals($hex_format, $esn->getHex());
+	}
+
+	/** @test */
+	public function returns_proper_dec_format_when_submitted_in_dec()
+	{
+		$meid_number = '268435456010201020';
+		$esn = new Esn($meid_number);
+
+		$this->assertEquals($meid_number, $esn->getDec());
+	}
+
+	/** @test */
+	public function returns_proper_dec_format_when_submitted_in_hex()
+	{
+		$meid_number = 'A00000009BA7BC';
+		$hex_format = '268435456010201020';
+		$esn = new Esn($meid_number);
+
+		$this->assertEquals($hex_format, $esn->getDec());
 	}
 }
